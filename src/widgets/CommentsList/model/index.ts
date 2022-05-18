b47878825ae1c9c1a28254bf8b4944c15199771f
MrKruten <mrkruten@mail.ru>
2022-05-18 11:11:44 +0700

    fix( CommentsList ): sort comments by time

--- src/widgets/CommentsList/model/index.ts
+++ src/widgets/CommentsList/model/index.ts
@@ -1,5 +1,8 @@
 import { CommentsModel } from "entities/Comment";
+import { Helpers } from "shared/lib";
 
 export const $publishedComments = CommentsModel.$comments.map((comments) =>
-  comments.filter((comment) => comment.status === "approved")
+  comments
+    .sort((prev, next) => Helpers.compareDates(prev.createdAt, next.createdAt))
+    .filter((comment) => comment.status === "approved")
 );
